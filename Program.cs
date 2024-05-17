fa18402d231698e89adeb67ab52309ba99715a00
duyd486 <duyd486@gmail.com>
2024-05-17 22:02:05 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("nhap n: ");
+        int n = int.Parse(Console.ReadLine());
+        int [,] arr;
+        arr = new int[n, n];
+        for (int i = 0; i < n; i++) {
+            Console.WriteLine("Nhap phan tu hang " + i + ": ");
+            for (int j = 0; j < n; j++) {
+                arr[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == j) {
+                    sum += arr[i, j];
+                }
+            }
+        }
+        Console.WriteLine("tong duong cheo chinh la: " + sum);
+    }
+}
